632638dbc54bab073a820c97ab7c866ad8543408
Juan Francisco Hernandez Camacho <23060120@uadeo.mx>
2026-05-15 00:51:22 -0600

    edited triple type info

--- scripts/00000 Plugins/00002 DMC_TripleTypes/00000 Config.rb
+++ scripts/00000 Plugins/00002 DMC_TripleTypes/00000 Config.rb
@@ -10,7 +10,30 @@ module DMC
     CREATURE_TYPE_3 = {
       [:battling, 0] => :flying,
       [:nibblet, 0] => :flying,
-      [:Lamplet, 0] => :steel
+      [:echobat, 0] => :flying,
+      [:sonarian, 0] => :flying,
+      [:xenobat, 0] => :flying,
+      [:overseer, 0] => :mind,
+      [:hellbat, 0] => :flying,
+      [:hellwing, 0] => :fire,
+      [:Lamplet, 0] => :steel,
+      [:amplet, 0] => :steel,
+      [:Luxion, 0] => :steel,
+      [:Wicklet, 0] => :luz,
+      [:Wicklabra, 0] => :luz,
+      [:lunaryx, 0] => :luz,
+      [:mourndelume, 0] => :dark,
+      [:Waxy, 0] => :luz,
+      [:hivewax, 0] => :luz,
+      [:meltqueen, 0] => :royal,
+      [:combuxtix, 0] => :poison,
+      [:primawurlm, 0] => :dragon,
+      [:toadrum, 0] => :music,
+      [:toadossal, 0] => :music,
+      [:sunshroom, 0] => :holy,
+      [:ironmula, 0] => :steel,
+      [:sanchariot, 0] => :royal,
+      [:burriesta, 0] => :dream,
     }
     CREATURE_TYPE_3.default = :__undef__
   end
