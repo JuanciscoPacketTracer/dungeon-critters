5b2514c0f0d7f1e74c141c521795379cf451bb36
Juan Francisco Hernandez Camacho <23060120@uadeo.mx>
2026-05-25 00:44:37 -0600

    Separated the triple type and summary revamps to be standalone plugins

--- scripts/00000 Plugins/00002 DMC_TripleTypes/00000 Config.rb
+++ scripts/00000 Plugins/00002 DMC_TripleTypes/00000 Config.rb
@@ -78,19 +78,3 @@ module DMC
     CREATURE_TYPE_3.default = :__undef__
   end
 end
-module DMC
-  module TypeRework
-    # Constant defining if you are using the rebalanced effectiveness calculation (true) or not (false)
-    MAINTYPE = true
-
-
-    # Constant defining the Type3 of a Pokemon by form
-    # @return [Hash{ Array<Symbol, Integer> => Symbol }
-    # Example: [:bulbasaur, 0] => :fire
-    CREATURE_TYPE_3 = {
-      [:bulbasaur, 0] => :fire,
-      [:charmander, 0] => :ghost
-    }
-    CREATURE_TYPE_3.default = :__undef__
-  end
-end
